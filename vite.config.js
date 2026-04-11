97da93f90201e93fa958070280d35babe866e681
shaton3012 <typo3012@gmail.com>
2026-04-11 12:34:58 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/neotetra/', 
+  // 以下の行を追加してください。これでCSSやJSの読み込み先が正しくなります。
+  base: '/neotetra/',
 })
