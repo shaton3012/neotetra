a95b2fbcb5117086cb9113076e6fd24e466ecd37
Naoki Matsuo <naoki@NaokinoMacBook-puro.local>
2026-04-11 11:19:50 +0900

    fix: add deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/neotetra/', //
+  base: '/neotetra/', // これが重要です
   plugins: [react()],
 })
