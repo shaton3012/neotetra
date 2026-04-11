c9167e296ddce02839b7e41141f44a15eb2ddf52
shaton3012 <typo3012@gmail.com>
2026-04-11 12:56:30 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pagesのサブディレクトリ用設定
-  base: '/neotetra/', 
+  // GitHub PagesのURL階層に合わせるための設定
+  base: '/neotetra/',
 })
